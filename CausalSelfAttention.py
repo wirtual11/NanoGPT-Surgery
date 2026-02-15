7b4a0e4f9e0d49b184c1e793d34510df62e3b264
Wirtual11 <22962454+wirtual11@users.noreply.github.com>
2026-02-15 22:08:07 +0000

    Project init - not complie

---
+++ CausalSelfAttention.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+class :
+    def __init__(self) -> None:
+        super().__init__()
+        
+        #Embedding dimension must be divisible by number of heads"
+        assert config.n_embd % config.n_head == 0
